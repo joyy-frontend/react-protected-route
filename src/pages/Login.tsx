832beac672a4c6fb1444027bb5eeb6b7779a217f
rhkk7157 <rhkk7157@gmail.com>
2023-08-31 17:05:18 +0900

    Login UI 생성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.post('/user/login', { id: id, pw: password }).then((res) => {
+      console.log(res)
+    })
+  }
+
+  return (  
+    <FormWrapper onSubmit={handleSubmit}>
+      <InputWrapper>
+        ID
+        <input 
+          type='text' 
+          autoFocus 
+          placeholder='아이디를 입력해주세요'
+          onChange={(e) => {
+            setId(e.target.value);
+          }}  
+        />
+      </InputWrapper>
+      <InputWrapper>
+        Password
+        <input 
+          type='password' 
+          autoFocus 
+          placeholder='비밀번호를 입력해주세요'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}  
+        />
+      </InputWrapper>
+      <Button type='submit'>로그인</Button>
+    </FormWrapper>
   )
-}
+};
+
+const FormWrapper = styled.form`
+  border: 3px solid red;
+  display: flex;
+  flex-direction: column;
+  padding: 56px;
+  border: 1px solid #eee;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+const InputWrapper = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  widdth: 100%;
+  & > input {
+    padding: 8px 16px;
+    border: 1px solid #eee;
+  }
+`;
+
+const Button = styled.button`
+  padding: 16px;
+  width: 100%;
+  background-color: #99ccff;
+  color: #fff;
+`;
 
 export default Login
