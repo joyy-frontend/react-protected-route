2ac611f22c0c22d4dfc0903427b0bc473cc6b794
rhkk7157 <rhkk7157@gmail.com>
2023-08-31 17:04:36 +0900

    msw setting

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,9 +1,16 @@
 import { rest } from 'msw';
 
-export const handlers = [
-  // Handles a POST /login request
-  rest.post('/login', null),
-
-  // Handles a GET /user request
-  rest.get('/user', null),
-]
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  rest.post('/user/login', (req, res, ctx) => {
+    if(req.body.id && req.body.pw) {
+      return res(
+        ctx.json({
+          accessToken: "테스트",
+        })
+      );
+    } else {
+      return res(ctx.status(400));
+    }
+  }),
+];
